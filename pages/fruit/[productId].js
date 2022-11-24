ecd28e4fbc8968abf620836a885a22746d3bef01
MuhammadBorhan <0317371981@student.wub.edu.bd>
2022-11-24 18:45:28 +0600

    add to cart section created

--- pages/fruit/[productId].js
+++ pages/fruit/[productId].js
@@ -1,19 +1,25 @@
-import { ArrowRight } from "@mui/icons-material";
+import {
+  ArrowBack,
+  ArrowBackIos,
+  ArrowLeft,
+  ArrowRight,
+} from "@mui/icons-material";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import Layout from "../../components/Layout.jsx";
 
 const SingleFruit = () => {
   const [size, setSize] = useState(1);
+  const [quantity, setQuantity] = useState(1);
   const router = useRouter();
-  const { fruitName } = router.query;
+  const { productId } = router.query;
 
   const [posts, setPosts] = useState({});
   //   const { image, name, price, des } = posts;
   console.log(posts);
 
   useEffect(() => {
-    fetch(`http://localhost:3000/api/products/${fruitName}`)
+    fetch(`http://localhost:3000/api/products/${productId}`)
       .then((res) => res.json())
       .then((data) => setPosts(data));
   }, []);
@@ -33,8 +39,10 @@ const SingleFruit = () => {
             </p>
             <p className="text-5xl font-bold">
               <span className="text-orange-600">$</span>
-              {posts.price[size]}
+              {posts.price}
             </p>
+
+            {/* Size Section */}
             <div className="flex items-center mt-6 mb-4 gap-8">
               <p className="text-3xl font-bold">
                 Size <ArrowRight />{" "}
@@ -66,6 +74,27 @@ const SingleFruit = () => {
                 </p>
               </div>
             </div>
+
+            {/* Quantity section */}
+            <div className="flex py-3 gap-2 items-center">
+              <h2 className="text-2xl font-bold">Quantity : </h2>
+              <div className="flex items-center">
+                <span
+                  onClick={() => {
+                    if (quantity > 1) setQuantity(quantity - 1);
+                  }}
+                >
+                  <ArrowLeft className="text-6xl cursor-pointer" />
+                </span>
+                <span className="text-3xl">{quantity}</span>
+                <span onClick={() => setQuantity(quantity + 1)}>
+                  <ArrowRight className="text-6xl cursor-pointer" />
+                </span>
+              </div>
+            </div>
+            <button className="px-4 py-2 bg-orange-600 text-xl font-bold rounded-full text-white mt-2">
+              Add To Cart
+            </button>
           </div>
         </div>
       </div>
