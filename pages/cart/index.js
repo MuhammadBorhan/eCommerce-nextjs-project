1f1821f26a6ac96744df8a2016a1514c969d6923
MuhammadBorhan <0317371981@student.wub.edu.bd>
2022-11-26 14:17:56 +0600

    so far so good

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -36,55 +36,65 @@ const index = () => {
   return (
     <Layout>
       <div>
-        {data?.map((item) => {
-          const { _id, name, image, des, quantity, size } = item;
-          return (
-            <div key={_id}>
-              <div className="p-12 flex flex-col md:flex-row items-center shadow-lg mx-0 md:mx-12">
-                <div>
-                  <img src={image} height={150} width={150} />
-                </div>
-                <div className="px-12">
-                  <h2 className="text-2xl font-bold">{name}</h2>
-                  {/* <p className="text-slate-500 font-bold my-2">
+        <div>
+          {data?.map((item) => {
+            const { _id, name, image, des, quantity, size } = item;
+            return (
+              <div key={_id}>
+                <div className="p-12 flex flex-col md:flex-row items-center justify-center shadow-lg mx-0 md:mx-12">
+                  <div className="mb-2 md:mb-0">
+                    <img src={image} height={150} width={150} />
+                  </div>
+                  <div className="px-12">
+                    <h2 className="text-2xl font-bold">{name}</h2>
+                    {/* <p className="text-slate-500 font-bold my-2">
                     {des?.length > 100 ? des.slice(0, 100) + " . . ." : des}
                   </p> */}
-                  {size === "0" ? (
-                    <p className="text-xl text-indigo-600 font-bold font-mono">
-                      small
+                    {size === "0" ? (
+                      <p className="text-xl text-indigo-600 font-bold font-mono">
+                        small
+                      </p>
+                    ) : size === "1" ? (
+                      <p className="text-xl text-indigo-600 font-bold font-mono">
+                        Medium
+                      </p>
+                    ) : (
+                      <p className="text-xl text-indigo-600 font-bold font-mono">
+                        Large
+                      </p>
+                    )}
+                    <p className="text-2xl font-bold font-mono">
+                      <span className="text-orange-600">Tk</span>
+                      {size === "0"
+                        ? price[0]
+                        : size === "1"
+                        ? price[1]
+                        : price[2]}
                     </p>
-                  ) : size === "1" ? (
-                    <p className="text-xl text-indigo-600 font-bold font-mono">
-                      Medium
+                    <p className="text-xl font-bold">Quantity: {quantity}</p>
+                    <p className="text-2xl font-bold font-mono">
+                      Total: <span className="text-orange-600">Tk</span>
+                      {size === "0"
+                        ? parseInt(quantity) * parseFloat(price[0])
+                        : size === "1"
+                        ? parseInt(quantity) * parseFloat(price[1])
+                        : parseInt(quantity) * parseFloat(price[2])}
                     </p>
-                  ) : (
-                    <p className="text-xl text-indigo-600 font-bold font-mono">
-                      Large
-                    </p>
-                  )}
-                  <p className="text-2xl font-bold font-mono">
-                    <span className="text-orange-600">Tk</span>
-                    {size === "0"
-                      ? price[0]
-                      : size === "1"
-                      ? price[1]
-                      : price[2]}
-                  </p>
-                  <p className="text-xl font-bold">Quantity: {quantity}</p>
-                  <p>Total: {parseInt(quantity) * parseFloat(price)}</p>
 
-                  <button
-                    onClick={() => handleRemove(_id)}
-                    className="flex mt-4 text-orange-600 font-bold text-xl"
-                  >
-                    Remove
-                  </button>
+                    <button
+                      onClick={() => handleRemove(_id)}
+                      className="flex mt-4 text-orange-600 font-bold text-xl"
+                    >
+                      Remove
+                    </button>
+                  </div>
                 </div>
               </div>
-            </div>
-          );
-        })}
-        <ToastContainer />
+            );
+          })}
+          <ToastContainer />
+        </div>
+        <div>right side</div>
       </div>
     </Layout>
   );
